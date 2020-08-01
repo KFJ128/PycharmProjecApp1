5f0b64da116f7f1a54e337195fee5159f0b9c6e7
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-07-31 21:15:02 -0500

    nueva_nota

--- NOTA_FINAL.py
+++ NOTA_FINAL.py
@@ -1,10 +1,11 @@
 nota_1 = int(input(" leer primera nota del estudiante: "))
 nota_2 = int(input(" leer segunda nota del estudiante: "))
 nota_3 = int(input(" leer tercera nota del estudiante: "))
+notaa_5 = int(input(" leer quinta nota del estudiante: "))
 nota_4 = int(input(" leer cuarta nota del estudiante: "))
-nota_5 = int(input(" leer quinta nota del estudiante: "))
-promedio = (nota_1 + nota_2 + nota_3 + nota_4 + nota_5) / 5
-if promedio >= 7:
+not_6 = int(input(" leer sexta nota del estudiante: "))
+proedio = (nota_1 + nota_2 + nota_3 + nota_4 + nota_5 + nota_6) / 6
+if prmomedio >= 7:
      print(" su promedio es de: ",  promedio, " por ende el estudiante pasa de año ")
 else:
      print(" su promedio es de:",  promedio,  "por ende el estudiante no pasa de año ")
