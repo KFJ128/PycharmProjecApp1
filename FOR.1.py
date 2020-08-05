e8cd988677100102907b2c73d6cee0ca6978d797
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-08-05 12:25:54 -0500

    for11

--- FOR.1.py
+++ FOR.1.py
@@ -1,2 +1,14 @@
 for i in range(20, 0, -1):
     print(" informes del año: ", i)
+#factorial de un numero
+factorial = int(input(" poner el valor a factorial: "))
+for i in range(1, factorial, 1):
+    factorial *= i
+    print(" factorial es igual: ", factorial)
+
+#cuadrado de un número
+n = int(input(" leer valor: "))
+respuesta = 0
+for i in range(1, 2 * n -1+1, 2):
+    respuesta += i
+    print(" cuadrado del número es:", respuesta)
