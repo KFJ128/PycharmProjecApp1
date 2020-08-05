2073c008453cf5ece30b196b3f4274360f2d0a43
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-08-05 11:54:33 -0500

    WHILE.1.

---
+++ WHILE.1.py
@@ -0,0 +1,17 @@
+i = 1  # inicializador del contador
+while i <= 100:  # condicion del bucle
+ print(" números del 1 al 100 ", i)
+ if i == 50:
+     break
+ i += 1  # incremento contador
+
+
+
+
+ #infomre del año
+anio = 2000
+while anio <= 2012:
+    print(" informes del año: ", anio)
+    anio += 1
+
+#practica en clases
