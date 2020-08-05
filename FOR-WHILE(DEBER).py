00eb25f6c50594bb9310ebfef802d74517246e2e
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-08-05 16:59:26 -0500

    for anD white

--- FOR-WHILE(DEBER).py
+++ FOR-WHILE(DEBER).py
@@ -12,7 +12,6 @@ print(" Aproximación a pi en el método producto de Wallis: ", aproximacion)
 
 
 
-
 #divir
 dividendo = int(input(" Leer el dividendo:"))
 divisor= int(input(" Leer el divisor:"))
@@ -25,6 +24,42 @@ print("Conciente: ", cociente)
 print(" Resto: ", dividendo)
 
 
+#E
+n = int(input(" leer valor: "))
+acum = 0
+factorial = 1
+i = 1
+while i <= n:
+    factorial *= i
+    i += 1
+    fac = 1 / factorial
+    acum += fac
+print(acum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  #numero primo
@@ -32,39 +67,38 @@ n = int(input("Leer valor: "))
 acum = 1
 contt = 1
 prom = 0
-for i in range(1, n + 1, 1):
+for i in range(1, n+1):
  cont = 0
- for j in range(1, n + 1, 1):
-  if i % j == 0:
-   cont += 1
-  if cont == 2:
-    acum += i
-    contt += 1
-prom = (acum / contt)
+for j in range(1, n+1):
+ if i % j == 0:
+  cont += 1
+ elif cont == 2:
+   acum += i
+   contt += 1
+   prom = (acum / contt)
 print(" Pemerdio de números primos:", prom)
 
 
 #nwe formula
-resultado = 0
 e = int(input(" Leer valor: "))
+resultado = 0
 f1 = 1
 f2 = 1
 n1 = 0
 n2 = 0
-for m in range(0, e, 1):
+for m in range(0, e):
  e = m
- n2 = (2 * e +1)
+ n2 = (2 * e) * 1
  n1 = e
- for i in range(1, n1, 1):
+ for i in range(1, n1):
   f1 *= i
- for j in range(1, n2, 1):
-  f2 = f2 * j
-
-resultado += ((2 ** e) * (f1) ** 2 / f2 )
+  for j in range(1, n2):
+   f2 = f2 * j
+resultado += ((2 ** e) * (f1 ** 2) / f2)
 f1 = 1
 f2 = 1
-
 print(resultado)
+
 
 
 
