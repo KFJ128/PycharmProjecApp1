89a7c3c7ab5076062cca6b8b25c749220bbb43b5
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-08-05 21:07:27 -0500

    for anD white

--- FOR-WHILE(DEBER).py
+++ FOR-WHILE(DEBER).py
@@ -42,11 +42,11 @@ n = int(input("Leer valor: "))
 acum = 1
 contt = 1
 prom = 0
-for i in range(1, n):
+for i in range(1, n+1):
     cont = 0
-    for j in range(1, n):
+    for j in range(1, n+1):
         if i % j == 0:
-         cont += 1
+            cont += 1
     if cont == 2:
        acum += i
        contt += 1
@@ -71,24 +71,24 @@ print(" Pemerdio de números primos:", prom)
 
 
 
-#nwe formula
+#new formula
 e = int(input(" Leer valor: "))
 resultado = 0
 f1 = 1
 f2 = 1
 n1 = 0
 n2 = 0
-for m in range(0, e):
- e = m
- n2 = (2 * e) * 1
- n1 = e
- for i in range(1, n1):
-  f1 *= i
-  for j in range(1, n2):
-   f2 = f2 * j
-resultado += ((2 ** e) * (f1 ** 2) / f2)
-f1 = 1
-f2 = 1
+for m in range(0, e, 1):
+    e = m
+    n2 = (2 * e) + 1
+    n1 = e
+    for i in range(1, n1+1, 1):
+        f1 *= i
+    for j in range(1, n2+1, 1):
+        f2 *= j
+    resultado += ((2 ** e) * (f1 ** 2)) / f2
+    f1 = 1
+    f2 = 1
 print(resultado)
 
 
