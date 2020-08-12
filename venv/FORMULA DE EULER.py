82a9775c46e736d9aa144371384aa3e55162d439
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-08-12 12:36:37 -0500

    factorial- vectores

--- venv/FORMULA DE EULER.py
+++ venv/FORMULA DE EULER.py
@@ -10,3 +10,24 @@ while i <= limite:
     sumatoria += formula
     i += 1
 print(" respuesta valor de E: ", sumatoria)
+
+
+#leer un vector de 5 elementos
+vector = []
+n = 5
+for i  in range(0, n):
+    vector.append(int(input(" ingrese el valor: ")))
+#mostrar valor del vector
+# 1.- manera
+for valor in vector:
+    print(" valor del vector:", valor)
+#2._ manera 2
+print(" el vector tiene ", len(vector), " elemento ")
+for i in range(0, len(vector)):
+    print(" elemento del vector ", i, " = ", vector[i])
+    #manera inversa
+print(" el vector tiene ", len(vector), " elemento ")
+
+
+for i in range(0, len(vector)-1, -1, -1):
+    print(" elemento del vector ", i, " = ", vector[i])
