fc17be5c93fc7ebc3fa5ef035dc704ef4abf9f0f
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-08-12 20:06:21 -0500

    new

--- FOR-WHILE(DEBER).py
+++ FOR-WHILE(DEBER).py
@@ -37,6 +37,22 @@ while i <= n:
 print(acum)
 
 
+#E2
+limite = int(input(" ingresar limite para aproximación de número E: "))
+sumatoria = 0
+formula = 1
+i = 0
+while i <= limite:
+    factorial = 1
+    for j in range(2, i +1):
+        factorial *= j
+    formula = 1 / factorial
+    sumatoria += formula
+    i += 1
+print(" respuesta valor de E: ", sumatoria)
+
+
+
  #numero primo
 n = int(input("Leer valor: "))
 acum = 1
