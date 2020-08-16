40e5f594565d14595f55ec555f0c1b037c71c5c2
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-08-15 21:14:42 -0500

    primo

--- venv/VECTORES.py
+++ venv/VECTORES.py
@@ -1,27 +1,6 @@
 
 
 
-#leer un vector de 5 elementos
-vector = []
-n = 5
-for i  in range(0, n):
-    vector.append(int(input(" ingrese el valor: ")))
-#mostrar valor del vector
-# 1.- manera
-for valor in vector:
-    print(" valor del vector:", valor)
-#2._ manera 2
-print(" el vector tiene ", len(vector), " elemento ")
-for i in range(0, len(vector)):
-    print(" elemento del vector ", i, " = ", vector[i])
-    #manera inversa
-print(" el vector tiene ", len(vector), " elemento ")
-
-
-for i in range(0, len(vector)-1, -1, -1):
-    print(" elemento del vector ", i, " = ", vector[i])
-
-
 
 
 
@@ -32,5 +11,23 @@ for i in range(0, n):
 print(" el vector tiene: ", len(vector1), " elementos ")
 for j in range(0, len(vector1)):
     print(" elemento del vector ", j,":",  vector1[j])
+
+
+
+
+#algoritmo pirmo o compuesto
+numero = int(input(" leer valor del número:"))
+numero2 = int(numero)
+numero_cuadrado = int(numero2 // (numero) ** (1 / 2))
+print("piso del numero: ", numero_cuadrado)
+for d in range(1, numero_cuadrado+1):
+    if  numero_cuadrado % d == 1:
+      print(" rango de numero compuesto de", numero_cuadrado, " % ", d, " es igual a: ",  numero_cuadrado % d)
+    else:
+     print(" rango de numero primos de", numero_cuadrado, " % ", d, " es igual a: ",  numero_cuadrado % d)
+if numero_cuadrado % d == 1:
+    print(" es un número compuesto ")
+else:
+    print(" es un número primo ")
 
 
