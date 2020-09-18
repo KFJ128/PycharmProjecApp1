bc5822bb55419a78522d1da70ebfda04330585dd
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-09-18 10:52:38 -0500

    PRUEBA FINAL!!! algoritmos.1

--- PRUEBA DEL SEGUNDO CORTE!!.py
+++ PRUEBA DEL SEGUNDO CORTE!!.py
@@ -4,3 +4,9 @@ def crearmatrizalealtoria(filas, columnas):
     return np.random.randint(1, 11, (filas, columnas))
 matriz_a = crearmatrizalealtoria(10, 10)
 print(matriz_a)
+matriz_a_vt = np.zeros((10, 1))
+for j in range(1, matriz_a[range(0, 10), range(0, 10)]):
+    for i in range(len(matriz_a)):
+        matriz_a_vt[i, 0] = matriz_a[0, i]
+    print(matriz_a_vt)
+
