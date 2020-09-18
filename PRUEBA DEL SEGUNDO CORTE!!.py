4eb138d5dfc1a20aa38447abcb5e9a217ba4cf14
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-09-18 11:20:44 -0500

    PRUEBA FINAL!!! algoritmos.1.3

--- PRUEBA DEL SEGUNDO CORTE!!.py
+++ PRUEBA DEL SEGUNDO CORTE!!.py
@@ -3,6 +3,7 @@ import numpy as np
 def crearmatrizalealtoria(filas, columnas):
     return np.random.randint(1, 11, (filas, columnas))
 matriz_a = crearmatrizalealtoria(10, 10)
+print("MATRIZ_A:")
 print(matriz_a)
 matriz_a_vt = np.zeros((10, 10))
 for i in range(len(matriz_a)):
@@ -10,4 +11,5 @@ for i in range(len(matriz_a)):
         matriz_a_vt[j, i] = matriz_a[i, j]
 for k in range(len(matriz_a_vt)):
     matriz_a_vt[k, k] = 0
+print("MATRIZ TRANSPUESTA DE A, CON DIAGONAL PRINCIPAL DE 0: ")
 print(matriz_a_vt)
