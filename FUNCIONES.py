4a5230ded11e7ded930fa527b91743b00d22801f
KEVINMJ <kjimenezb3@uteq.edu.ec>
2020-09-22 13:07:48 -0500

    vt[fila, columna] =[columna, fila]:

--- FUNCIONES.py
+++ FUNCIONES.py
@@ -9,15 +9,16 @@ w = np.zeros((1, 6))
 vt = np.zeros((6, 1))
 for i in range(len(A)):
     v[0, i] = np.sum(A[i, :] ** 2) ** 0.5
+print("primer vector: ")
 print(v)
 
-for i in range(len(A)):
-    w[0, i] = np.sum(A[:, i] ** 3) ** 0.333333
+for j in range(len(A)):
+    w[0, j] = np.sum(A[:, j] ** 3) ** 0.333333
 print(w)
 
 
-for i in range(len(A)):
-    vt[i, 0] = v[0, i]
+for k in range(len(A)):
+    vt[k, 0] = v[0, k]
 print(vt)
 s = 1 / np.dot(w, vt) * np.dot(vt, w)
 print(s)
